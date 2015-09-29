8272da7a1ed395de15780a0a523d4d15616f6be0
Christopher Buck <cmbuck@vt.edu>
2015-09-29 17:10:30 -0500

    Test to illuminate segments statically

--- top.v
+++ top.v
@@ -19,7 +19,33 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module top(
+	clk,
+	blah_seg,
+	blah_an,
+	rst,
+	an[1:3],
+	seg[1:7],
+	in1[0:3],
+	in2[0:3]
     );
 
+input clk;
+output reg blah_seg;
+output reg blah_an;
+input rst;
+output [1:3] an;
+output [1:7] seg;
+input [0:3] in1;
+input [0:3] in2;
+
+assign an[1:3] = 3'b111;
+assign seg[1:7] = 7'b0000001;
+
+
+always @(posedge clk)
+begin
+	blah_seg <= 0;
+	blah_an <= 0;
+end
 
 endmodule
