b3c4feb4e2dff58b92b6ff4d3df7d09542777fc2
chewy441014 <plh1@rice.edu>
2015-10-06 17:16:47 -0500

    Added case for 7-segment multiplexing

--- top.v
+++ top.v
@@ -29,6 +29,7 @@ input clk;
 input rst;
 output reg [3:0] an;
 output reg [7:0] seg;
+reg [1:0] state;
 
 always @(posedge clk)
 begin
@@ -37,11 +38,30 @@ begin
 		an[3:0] <= 4'b1111;
 		seg[7:0] <= 8'b11111111;
 	end
-	else
+	else	//display on the seg
 	begin
+		case (state)
+		0 : begin 
+			state <= 1; 
+			an[3:0] <= 4'b0001; 
+			end
+		1 : begin 
+			state <= 2; 
+			an[3:0] <= 4'b0010; 
+			end
+		2 : begin 
+			state <= 3; 
+			an[3:0] <= 4'b0100; 
+			end
+		3 : begin 
+			state <= 0; 
+			an[3:0] <= 4'b1000; 
+			end
 		an[3:0] <= 4'b1110;
 		seg[7:0] <= 8'b00001111;
+		endcase
 	end
+	seg <= 8'b10101010;
 end
 
 endmodule
