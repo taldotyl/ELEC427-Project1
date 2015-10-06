b1a89eab45c85a0a8bebd0e20ab5d45cfbe34c12
unknown <tnt1@ABC-A124-05-960.ad.rice.edu>
2015-10-06 18:04:20 -0500

    Upload of top.v, tb_top.v, and the ucf file

---
+++ tb_top.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:45:51 10/06/2015
+// Design Name:   top
+// Module Name:   U:/ELEC427/Proj1/Project1/tb_top.v
+// Project Name:  Project1
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: top
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module tb_top;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg startstop;
+	reg up;
+	reg down;
+
+	// Outputs
+	wire [3:0] an;
+	wire [7:0] seg;
+
+	// Instantiate the Unit Under Test (UUT)
+	top uut (
+		.clk(clk), 
+		.rst(rst), 
+		.an(an), 
+		.seg(seg), 
+		.startstop(startstop), 
+		.up(up), 
+		.down(down)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		startstop = 0;
+		up = 0;
+		down = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+		rst = 1;
+		#20;
+		rst = 0;
+		#10;
+		
+	
+		
+	end
+	always    
+		#5 clk = !clk;
+      
+endmodule
+
--- top.v
+++ top.v
@@ -22,11 +22,17 @@ module top(
 	clk,
 	rst,
 	an[3:0],
-	seg[7:0]
+	seg[7:0],
+	startstop,
+	up,
+	down
     );
 
 input clk;
 input rst;
+input startstop;
+input up;
+input down;
 output reg [3:0] an;
 output reg [7:0] seg;
 reg [1:0] state;
@@ -37,31 +43,33 @@ begin
 	begin
 		an[3:0] <= 4'b1111;
 		seg[7:0] <= 8'b11111111;
+		state <= 0;
 	end
 	else	//display on the seg
 	begin
 		case (state)
 		0 : begin 
 			state <= 1; 
-			an[3:0] <= 4'b0001; 
+			an[3:0] <= 4'b1110;
+			seg[7:0] <= 8'b10110000;
 			end
 		1 : begin 
 			state <= 2; 
-			an[3:0] <= 4'b0010; 
+			an[3:0] <= 4'b1101; 
+			seg[7:0] <= 8'b10000000;
 			end
 		2 : begin 
 			state <= 3; 
-			an[3:0] <= 4'b0100; 
+			an[3:0] <= 4'b1011; 
 			end
 		3 : begin 
 			state <= 0; 
-			an[3:0] <= 4'b1000; 
+			an[3:0] <= 4'b0111; 
 			end
-		an[3:0] <= 4'b1110;
-		seg[7:0] <= 8'b00001111;
-		endcase
+			endcase
+		
 	end
-	seg <= 8'b10101010;
+
 end
 
 endmodule
